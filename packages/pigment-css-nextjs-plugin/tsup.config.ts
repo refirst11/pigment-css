8f1fab6da38a75fb456632932525b77056676c86
refirst11 <ocelot2828@gmail.com>
2026-06-05 13:27:00 +0900

    [nextjs] Add turbopack-loader build setting

--- packages/pigment-css-nextjs-plugin/tsup.config.ts
+++ packages/pigment-css-nextjs-plugin/tsup.config.ts
@@ -3,11 +3,21 @@ import config from '../../tsup.config';
 
 const configOptions = config as Options;
 
-const baseConfig: Options = {
-  ...configOptions,
-  tsconfig: './tsconfig.build.json',
-  cjsInterop: true,
-  entry: ['./src/index.ts', './src/virtual-css-loader.js'],
-};
-
-export default defineConfig(baseConfig);
+export default defineConfig([
+  {
+    ...configOptions,
+    tsconfig: './tsconfig.build.json',
+    cjsInterop: true,
+    entry: ['./src/index.ts', './src/virtual-css-loader.js'],
+  },
+  {
+    ...configOptions,
+    tsconfig: './tsconfig.build.json',
+    cjsInterop: false,
+    splitting: false,
+    format: ['cjs'],
+    entry: {
+      'turbopack-loader': './src/turbopack-loader.ts',
+    },
+  },
+]);
